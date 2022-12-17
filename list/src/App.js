7858531b35ed73cb8cc8ecd9a1141992c4fd21a1
nandini <nandinikashyap1902@gmail.com>
2022-12-17 19:44:13 +0530

    first commit

---
+++ list/src/App.js
@@ -0,0 +1,17 @@
+import './app.css';
+function App() {
+  return (
+    <div className="App">
+      <h1>Top 5 Netflix Movies</h1>
+      <ul>
+        <li><a href="https://en.wikipedia.org/wiki/Bullet_Train_(film)">Bullet Train (2022)</a></li>
+<li><a href="https://en.wikipedia.org/wiki/Troll_(2022_film)">Troll (2022) </a></li>
+<li><a href="https://en.wikipedia.org/wiki/Emily_the_Criminal">Emily the Criminal (2022)</a> </li>
+<li><a href="https://en.wikipedia.org/wiki/Lady_Chatterley%27s_Lover">Lady Chatterley's Lover (2022)</a> </li>
+<li><a href="https://en.wikipedia.org/wiki/Storks_(film)">Storks (2016)</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
